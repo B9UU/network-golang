32fdec38adbb588b015bde04ae70da1e5c42c4a6
brahim <brahim.boussaa1998@gmail.com>
2024-07-10 15:34:53 +0100

    add unmarshalBinary to ReadReq

--- tftp/types.go
+++ tftp/types.go
@@ -3,6 +3,8 @@ package tftp
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
+	"strings"
 )
 
 const (
@@ -38,16 +40,20 @@ type ReadReq struct {
 	Mode     string
 }
 
-// Reads the request packet structure
+// Writes the request packet structure
 // 2 bytes - opCode | n bytes - filename | 1 byte - 0 | n byte - mode | 1 byte - 0
+//
+//	WARNING:   I don't we shoud add 4 bytes for OpCode
 func (q ReadReq) MarshalBinary() ([]byte, error) {
 	mode := "octet"
 	if q.Mode != "" {
 		mode = q.Mode
 	}
+	// Here ----------
 	cap := 2 + 2 + len(q.Filename) + 1 + len(q.Mode) + 1
 	b := new(bytes.Buffer)
 	b.Grow(cap)
+
 	err := binary.Write(b, binary.BigEndian, OpRRQ)
 	if err != nil {
 		return nil, err
@@ -69,4 +75,39 @@ func (q ReadReq) MarshalBinary() ([]byte, error) {
 		return nil, err
 	}
 	return b.Bytes(), nil
+}
+
+// Reads the request packet structure
+// 2 bytes - opCode | n bytes - filename | 1 byte - 0 | n byte - mode | 1 byte - 0
+func (q *ReadReq) UnmarshalBinary(p []byte) error {
+	r := bytes.NewBuffer(p)
+	var code OpCode
+	err := binary.Read(r, binary.BigEndian, &code)
+	if err != nil {
+		return err
+	}
+	if code != OpRRQ {
+		return errors.New("invalid RRQ")
+	}
+	q.Filename, err = r.ReadString(0)
+	if err != nil {
+		return errors.New("invalid RRQ")
+	}
+	q.Filename = strings.TrimRight(q.Filename, "\x00")
+	if len(q.Filename) == 0 {
+		return errors.New("invalid RRQ")
+	}
+	q.Mode, err = r.ReadString(0)
+	if err != nil {
+		return errors.New("invalid RRQ")
+	}
+	q.Mode = strings.TrimRight(q.Mode, "\x00")
+	if len(q.Filename) == 0 {
+		return errors.New("invalid RRQ")
+	}
+	actual := strings.ToLower(q.Mode)
+	if actual != "octet" {
+		return errors.New("only binary transfers supported")
+	}
+	return nil
 }

436dbb1dcf18a88ae3c8bf913d62707bb7448d42
brahim <brahim.boussaa1998@gmail.com>
2024-07-24 02:50:48 +0100

    add postusertest

---
+++ chttp/post_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type User struct {
+	First string
+	Last  string
+}
+
+func handlePostUser(t *testing.T) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		defer func(r io.ReadCloser) {
+			_, _ = io.Copy(io.Discard, r)
+			_ = r.Close()
+		}(r.Body)
+		if r.Method != http.MethodPost {
+			http.Error(w, "", http.StatusMethodNotAllowed)
+			return
+		}
+		var u User
+		err := json.NewDecoder(r.Body).Decode(&u)
+		if err != nil {
+			t.Error(err)
+			http.Error(w, "Decode Failed", http.StatusBadRequest)
+			return
+		}
+		w.WriteHeader(http.StatusAccepted)
+	}
+}
+
+func TestPostUser(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(handlePostUser(t)))
+	defer ts.Close()
+	resp, err := http.Get(ts.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusMethodNotAllowed {
+		t.Fatalf("expected status %d; actual status %d",
+			http.StatusMethodNotAllowed, resp.StatusCode)
+	}
+	buf := new(bytes.Buffer)
+	user := User{First: "laqo", Last: "Chaqo"}
+	err = json.NewEncoder(buf).Encode(&user)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp, err = http.Post(ts.URL, "application/json", buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusAccepted {
+		t.Fatalf("expected status %d; actual status %d",
+			http.StatusAccepted, resp.StatusCode)
+	}
+	_ = resp.Body.Close()
+}
